88a7a073597991b6c2ca574fa067c89ffc38c3d2
Calvin Cushman and Hannah Kinne <calvin@>
2017-04-25 11:08:30 -0700

    add a button to change background to black and text to white.

--- walrus/js/scripts.js
+++ walrus/js/scripts.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     $(".walrus-showing").fadeToggle();
     $(".walrus-hidden").fadeToggle();
   });
+
+  $("button#black").click(function() {
+    $("body").addClass("black-background");
+  });
 });
